319ece09bc0a88491d6edc7dc54ca6ecc168f840
Bizimana_William_Gad <weeleegad@gmail.com>
2023-06-30 08:09:07 +0000

    Startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,40 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomePage extends StatelessWidget {
+  final CollectionReference playersCollection =
+      FirebaseFirestore.instance.collection('players');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Soccer Records'),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Add code to fetch and display player records
+              },
+              child: Text('Show Player Records'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add code to add a new player record
+              },
+              child: Text('Add Player Record'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add code to sign out
+              },
+              child: Text('Sign Out'),
+            ),
+          ],
         ),
       ),
     );
